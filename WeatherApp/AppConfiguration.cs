ccd157f802a95862461e5f229446e105a7f887e3
Potato <raph4928@gmail.com>
2021-11-08 21:39:55 -0500

    AppConfiguration complété - 1949141

---
+++ WeatherApp/AppConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public static class AppConfiguration
+    {
+        private static IConfiguration configuration;
+        private static void initConfig()
+        {
+           
+            configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddUserSecrets("5c3c345e-f5a5-497d-8b70-44f2326e4861").Build();
+        }
+        public static string GetValue(string key)
+        {
+            if (configuration == null)
+            {
+                initConfig();
+            }
+            return configuration[key];
+        }
+    }
+}
